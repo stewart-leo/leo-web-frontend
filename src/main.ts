83e0a105a5c4d0b6d87d914e3e7015e8c4d047d8
Stewart Lait <stewartlait@leadingedgeonly.com>
2025-09-14 20:39:22 +0100

    Step2

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,13 @@ import { Form } from '@primevue/forms'
 
 import AutoComplete from 'primevue/autocomplete'
 import Button from 'primevue/button'
+import Checkbox from 'primevue/checkbox'
+import CheckboxGroup from 'primevue/checkboxgroup'
+import Fieldset from 'primevue/fieldset'
 import FloatLabel from 'primevue/floatlabel'
 import InputText from 'primevue/inputtext'
 import InputNumber from 'primevue/inputnumber'
+import MultiSelect from 'primevue/multiselect'
 import Panel from 'primevue/panel'
 import RadioButton from 'primevue/radiobutton'
 import Select from 'primevue/select'
@@ -44,10 +48,14 @@ app.use(PrimeVue, {
 // with other libraries or custom components.
 app.component('PvAutoComplete', AutoComplete)
 app.component('PvButton', Button)
+app.component('PvCheckbox', Checkbox)
+app.component('PvCheckboxGroup', CheckboxGroup)
+app.component('PvFieldset', Fieldset)
 app.component('PvFloatLabel', FloatLabel)
 app.component('PvForm', Form)
 app.component('PvInputText', InputText)
 app.component('PvInputNumber', InputNumber)
+app.component('PvMultiSelect', MultiSelect)
 app.component('PvPanel', Panel)
 app.component('PvRadioButton', RadioButton)
 app.component('PvSelect', Select)
@@ -56,6 +64,6 @@ app.component('PvStepper', Stepper)
 app.component('PvStepList', StepList)
 app.component('PvStepPanel', StepPanel)
 app.component('PvStepPanels', StepPanels)
-app.component('PvTextarea', Textarea)
+app.component('PvTextArea', Textarea)
 
 app.mount('#app')
